2cdca0867cc568a5cfa6ee552059a6869d9c29d6
virender bhargav <virender@grappus.com>
2018-01-21 20:34:23 +0530

    setting updated

--- src/mysocial/settings.py
+++ src/mysocial/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if config('DEBUG') in ['FALSE', False, None] else True
 # '::::' seperated string for allowed hosts
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split('::::') if config('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
