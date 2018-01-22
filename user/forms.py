b7792a4f8cb7f4270f9734172319378631703244
virender bhargav <virender@grappus.com>
2018-01-22 06:28:51 +0530

    template updated

--- user/forms.py
+++ user/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-text'}), label='username', max_length=150)
-    password = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-text'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input100',
+                                      'placeholder': 'Type your username',
+                                      'type': 'text',}), label='username', max_length=150)
+    password = forms.CharField(widget=forms.TextInput(attrs={'class': 'input100',
+                                      'placeholder': 'Type your password',
+                                      'type': 'password',}))
